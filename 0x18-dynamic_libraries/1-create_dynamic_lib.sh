e8c34019da7ab437085965553b4edaa385b6a522
MouadAmmi <mouadammi0@gmail.com>
2023-11-15 16:55:52 -0800

    this

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Compile all .c files into a dynamic library named liball.so
+gcc -shared -o liball.so *.c
+
---
+++ 0x18-dynamic_libraries/libdynamic.c
@@ -0,0 +1,101 @@
+// libdynamic.c
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char c) {
+    return write(1, &c, 1);
+}
+
+int _islower(int c) {
+    // Implementation for _islower
+    return 0;
+}
+
+int _isalpha(int c) {
+    // Implementation for _isalpha
+    return 0;
+}
+
+int _abs(int n) {
+    // Implementation for _abs
+    return 0;
+}
+
+int _isupper(int c) {
+    // Implementation for _isupper
+    return 0;
+}
+
+int _isdigit(int c) {
+    // Implementation for _isdigit
+    return 0;
+}
+
+int _strlen(char *s) {
+    // Implementation for _strlen
+    return 0;
+}
+
+void _puts(char *s) {
+    // Implementation for _puts
+}
+
+char *_strcpy(char *dest, char *src) {
+    // Implementation for _strcpy
+    return dest;
+}
+
+int _atoi(char *s) {
+    // Implementation for _atoi
+    return 0;
+}
+
+char *_strcat(char *dest, char *src) {
+    // Implementation for _strcat
+    return dest;
+}
+
+char *_strncat(char *dest, char *src, int n) {
+    // Implementation for _strncat
+    return dest;
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+    // Implementation for _strncpy
+    return dest;
+}
+
+int _strcmp(char *s1, char *s2) {
+    // Implementation for _strcmp
+    return 0;
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+    // Implementation for _memset
+    return s;
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    // Implementation for _memcpy
+    return dest;
+}
+
+char *_strchr(char *s, char c) {
+    // Implementation for _strchr
+    return NULL;
+}
+
+unsigned int _strspn(char *s, char *accept) {
+    // Implementation for _strspn
+    return 0;
+}
+
+char *_strpbrk(char *s, char *accept) {
+    // Implementation for _strpbrk
+    return NULL;
+}
+
+char *_strstr(char *haystack, char *needle) {
+    // Implementation for _strstr
+    return NULL;
+}
